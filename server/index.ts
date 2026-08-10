420ac45751389866480effa5cb004e70958618ce
yuming-lin <yuming-lin@Nuco.co.jp>
2026-08-10 12:58:03 +0900

    fix: 変数の名称を修正

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 app.use(express.json());
-const todos: string[] = [];
+const todoList: string[] = [];
 
 app.listen( 3001, () => {
   console.log("Server is running on port 3001");
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/todos", (req, res) => {
-  res.json(todos);
+  res.json(todoList);
 });
 
 app.post("/api/todos", (req, res) => {
@@ -21,6 +21,6 @@ app.post("/api/todos", (req, res) => {
     if (!taskContent || typeof taskContent !== "string" || taskContent.trim() === "") {
         return res.status(400).json({ error: "Invalid taskContent content" });
     }
-    todos.push(taskContent);
+    todoList.push(taskContent);
     res.status(201).json({ message: "Todo added" });
 }); 
