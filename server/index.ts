cb2d12dad1df32c70fb4c778104a38be19584e6e
yuming-lin <yuming-lin@Nuco.co.jp>
2026-08-12 15:47:45 +0900

    fix: todoListをデータベースに保存するように

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import fs from "fs/promises";
-import path from "path";
+// import fs from "fs/promises";
+// import path from "path";
 import cors from "cors"; // CORS（Cross-Origin Resource Sharing）を有効にするため //オリジン間リソース共有 //跨來源資源共享
 import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
@@ -18,36 +18,65 @@ const adapter = new PrismaBetterSqlite3({
 });
 const prisma = new PrismaClient({ adapter });
 
-const dataFilePath = path.join(process.cwd(), "todoList.json");
+// const dataFilePath = path.join(process.cwd(), "todoList.json");
 
-async function getTodoListFromFile(): Promise<string[]> {
+// async function getTodoListFromFile(): Promise<string[]> {
+//   try {
+//     const todoListData = await fs.readFile(dataFilePath, "utf-8");
+//     return JSON.parse(todoListData);
+//   } catch (error: any) {
+//     if (error.code === "ENOENT") {
+//       // ENOENT はファイルが存在しない場合のエラーコード
+//       return []; // プロジェクトが初めて実行される場合、空の配列を返す
+//     }
+//     console.error("エラー：todoList.jsonを読み取れませんでした", error);
+//     throw new Error("データの読み込み中にエラーが発生しました" + error.message);
+//   }
+// }
+
+async function getTodoListFromDB() {
   try {
-    const todoListData = await fs.readFile(dataFilePath, "utf-8");
-    return JSON.parse(todoListData);
+    const todoListDataInDB = await prisma.todo.findMany();
+    const todoList: string[] = todoListDataInDB.map((todo) => todo.content);
+    return todoList;
+
   } catch (error: any) {
-    if (error.code === "ENOENT") {
-      // ENOENT はファイルが存在しない場合のエラーコード
-      return []; // プロジェクトが初めて実行される場合、空の配列を返す
-    }
-    console.error("エラー：todoList.jsonを読み取れませんでした", error);
-    throw new Error("データの読み込み中にエラーが発生しました" + error.message);
+    console.error("エラー：データベースを読み取れませんでした", error);
+    throw new Error(
+      "データベースの読み込み中にエラーが発生しました" + error.message,
+    );
   }
 }
 
-async function saveTodoListToFile(taskList: string[]): Promise<void> {
-  try {
-    await fs.writeFile(
-      dataFilePath,
-      JSON.stringify(taskList, null, 2),
-      "utf-8",
-    );
+// async function saveTodoListToFile(taskList: string[]): Promise<void> {
+//   try {
+//     await fs.writeFile(
+//       dataFilePath,
+//       JSON.stringify(taskList, null, 2),
+//       "utf-8",
+//     );
 
-    // JSON.stringify(データ, 置換関数, インデント)
-    // - 第2引数 (null): フィルター（置換関数）は使用せず、すべてのデータをそのまま出力する
-    // - 第3引数 (2): インデント（字下げ）のスペース数を2個にし、ファイルを見やすく整形する
+//     // JSON.stringify(データ, 置換関数, インデント)
+//     // - 第2引数 (null): フィルター（置換関数）は使用せず、すべてのデータをそのまま出力する
+//     // - 第3引数 (2): インデント（字下げ）のスペース数を2個にし、ファイルを見やすく整形する
+//   } catch (error: any) {
+//     console.error("エラー：todoList.jsonを書き込めませんでした", error);
+//     throw new Error("データの書き込み中にエラーが発生しました" + error.message);
+//   }
+// }
+
+async function saveTodoListToDB(newTask: string) {
+  try {
+    await prisma.todo.create({
+      data: {
+        content: newTask,
+      },
+    });
   } catch (error: any) {
-    console.error("エラー：todoList.jsonを書き込めませんでした", error);
-    throw new Error("データの書き込み中にエラーが発生しました" + error.message);
+    console.error("エラー：デーラベーすに書き込めませんでした", error);
+    throw new Error(
+      "データベースに書き込み中にエラーが発生しました" + error.message,
+    );
   }
 }
 
@@ -57,7 +86,7 @@ app.get("/", (req, res) => {
 
 // GET API：ファイルからタスクリストを取得する
 app.get("/api/todos", async (req, res) => {
-  const todoList = await getTodoListFromFile();
+  const todoList = await getTodoListFromDB();
   res.json(todoList);
 });
 
@@ -71,12 +100,9 @@ app.post("/api/todos", async (req, res) => {
   ) {
     return res.status(400).json({ error: "Invalid taskContent content" });
   }
-  // jsonファイルは直接新しいデータを追加することはできないため
-  // まずは既存の todoList データを取得し、新しい task を追加してから保存する
-  const todoList = await getTodoListFromFile();
-  todoList.push(taskContent);
-  await saveTodoListToFile(todoList);
-  res.status(201).json(todoList);
+  await saveTodoListToDB(taskContent);
+  const currentTodoList = await getTodoListFromDB();
+  res.status(201).json(currentTodoList);
 });
 
 app.listen(3001, () => {
